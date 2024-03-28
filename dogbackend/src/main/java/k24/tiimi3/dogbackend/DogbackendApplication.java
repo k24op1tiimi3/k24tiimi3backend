c7fd6f90979e90ae506a2a7706a45a2d972727a9
Ponki Phan <ponkiphan@gmail.com>
2024-03-28 13:06:49 +0200

    Backend projektin luonti

---
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/DogbackendApplication.java
@@ -0,0 +1,13 @@
+package k24.tiimi3.dogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogbackendApplication.class, args);
+	}
+
+}
---
+++ dogbackend/src/test/java/k24/tiimi3/dogbackend/DogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package k24.tiimi3.dogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
