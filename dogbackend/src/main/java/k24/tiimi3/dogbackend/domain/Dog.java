c807d044a5c3d5451f003cf78551a55fd1db02fc
Kylebbos <kasperikuusisto@gmail.com>
2024-04-04 11:38:31 +0300

    Added bunch of stuff

--- dogbackend/src/main/java/k24/tiimi3/dogbackend/domain/Dog.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/domain/Dog.java
@@ -1,5 +1,108 @@
 package k24.tiimi3.dogbackend.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
+    public Dog(String title, String author, int publicationYear, String isbn, double price,
+            Category category) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Dog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        if (this.category != null) {
+            return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+                    + ", isbn=" + isbn
+                    + ", price=" + price + ", category=" + this.getCategory() + "]";
+        } else {
+            return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+                    + ", isbn=" + isbn
+                    + ", price=" + price + "]";
+        }
+    }
 
 }
--- dogbackend/src/main/java/k24/tiimi3/dogbackend/web/DogController.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/web/DogController.java
@@ -1,5 +1,14 @@
 package k24.tiimi3.dogbackend.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DogController {
+
+    @GetMapping("/*")
+    public String GetDog() {
+        return "";
+    }
 
 }
