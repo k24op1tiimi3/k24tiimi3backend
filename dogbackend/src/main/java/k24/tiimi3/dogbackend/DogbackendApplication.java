bff44c09d732192291394be7b11fb380ea9dc5bd
Kylebbos <kasperikuusisto@gmail.com>
2024-04-04 13:00:08 +0300

    added first category

--- dogbackend/src/main/java/k24/tiimi3/dogbackend/DogbackendApplication.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/DogbackendApplication.java
@@ -1,13 +1,27 @@
 package k24.tiimi3.dogbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import k24.tiimi3.dogbackend.domain.Category;
+import k24.tiimi3.dogbackend.domain.CategoryRepository;
 
 @SpringBootApplication
 public class DogbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DogbackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner kilometritDemo(CategoryRepository CategoryRepository) {
+		return (args) -> {
+
+			CategoryRepository.save(new Category("Jackets"));
+
+		};
 	}
 
 }
