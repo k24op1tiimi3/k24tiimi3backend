b4205c20bbe47f1a55cadc3ae038d20eff1a0714
wasabepeanut <wasabepeanut@gmail.com>
2024-04-16 21:29:23 +0300

    SpringBootTest, Couple tests added

--- dogbackend/src/test/java/k24/tiimi3/dogbackend/DogbackendApplicationTests.java
+++ dogbackend/src/test/java/k24/tiimi3/dogbackend/DogbackendApplicationTests.java
@@ -1,13 +1,74 @@
 package k24.tiimi3.dogbackend;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.isNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import k24.tiimi3.dogbackend.domain.Category;
+import k24.tiimi3.dogbackend.domain.CategoryRepository;
+import k24.tiimi3.dogbackend.domain.Dog;
+import k24.tiimi3.dogbackend.domain.DogRepository;
+import k24.tiimi3.dogbackend.domain.Manufacturer;
+import k24.tiimi3.dogbackend.domain.ManufacturerRepository;
 
 @SpringBootTest
 class DogbackendApplicationTests {
+
+
+	@Autowired
+	private DogRepository dogRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private ManufacturerRepository manufacturerRepository;
+
 
 	@Test
 	void contextLoads() {
 	}
 
+
+	// Trying to find data saved inside the Repository
+	@Test
+	void dataFoundInRepository() {
+		dogRepository.findAll();
+		categoryRepository.findAll();
+		manufacturerRepository.findAll();
+	}
+
+	// Creating a new entity and saving the entity to our DogRepository
+	@Test
+	void insertingNewEntityToRepository() {
+
+		Category category = new Category("Socks");
+		categoryRepository.save(category);
+
+		Manufacturer manufacturer = new Manufacturer("Gucci");
+		manufacturerRepository.save(manufacturer);
+
+		Dog dog = new Dog("Striped Gucci Socks", "Red/Green", "M",
+		 12.90, manufacturer, category);
+
+		dogRepository.save(dog);
+
+		// Saving our created dog from Repository. 
+		Dog savedDog = dogRepository.findById(dog.getId()).get();
+
+			// Assertions
+		// Checking that our dog is saved in dogRepository
+		assertNotNull(savedDog);
+
+		// Checking the components in savedDog match with our new Dog()
+		assertEquals("Striped Gucci Socks", savedDog.getTitle());
+		assertEquals("Red/Green", savedDog.getColor());
+		assertEquals("M", savedDog.getSize());
+		assertEquals(12.90, savedDog.getPrice());
+		assertEquals("Socks", savedDog.getCategory().getName());
+		assertEquals("Gucci", savedDog.getManufacturer().getName());
+	}
 }
