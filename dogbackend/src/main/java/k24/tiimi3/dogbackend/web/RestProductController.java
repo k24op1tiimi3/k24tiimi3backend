1288d7f7317cc0c39fdc1ac30d88a50047440439
Ponki Phan <ponkiphan@gmail.com>
2024-04-25 11:31:25 +0300

    CrossOrigin added

--- dogbackend/src/main/java/k24/tiimi3/dogbackend/web/RestProductController.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/web/RestProductController.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import k24.tiimi3.dogbackend.domain.ProductRepository;
+<<<<<<< Updated upstream
 import k24.tiimi3.dogbackend.domain.Type;
 import k24.tiimi3.dogbackend.domain.TypeRepository;
+=======
+import k24.tiimi3.dogbackend.domain.CategoryRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.GetMapping;
 import k24.tiimi3.dogbackend.domain.Product;
+
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/api")
