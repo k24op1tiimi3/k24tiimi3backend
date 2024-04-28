6c55a490f7f40e7178ff97b428b3aba53f9efd8a
whatmurder <jonasrinne@proton.me>
2024-04-28 15:28:04 +0300

    Added comments

--- dogbackend/src/main/java/k24/tiimi3/dogbackend/web/RestUserController.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/web/RestUserController.java
@@ -1,27 +1,27 @@
 package k24.tiimi3.dogbackend.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import k24.tiimi3.dogbackend.domain.AppUser;
 import k24.tiimi3.dogbackend.domain.AppUserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins =  "*")
+@CrossOrigin(origins = "*")
 
+
+// http://localhost:8080/api/appusers
 @RestController
 @RequestMapping("/api")
 public class RestUserController {
-    
+
     @Autowired
     private AppUserRepository userRepository;
 
     @GetMapping("/appusers")
-    public Iterable<AppUser> getMethodName(){
+    public Iterable<AppUser> getMethodName() {
         return userRepository.findAll();
     }
-    
+
 }
