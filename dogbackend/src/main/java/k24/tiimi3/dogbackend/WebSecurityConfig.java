2e143346abc2c0d4abcedd44746d5617bfff86ba
whatmurder <jonasrinne@proton.me>
2024-04-29 16:29:44 +0300

    Possible CORS tweaks, commented out for now.

--- dogbackend/src/main/java/k24/tiimi3/dogbackend/WebSecurityConfig.java
+++ dogbackend/src/main/java/k24/tiimi3/dogbackend/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package k24.tiimi3.dogbackend;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
+import k24.tiimi3.dogbackend.web.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.web.cors.*;
 
-import k24.tiimi3.dogbackend.web.UserDetailServiceImpl;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -19,11 +19,12 @@ public class WebSecurityConfig {
     @Autowired
     private UserDetailServiceImpl userDetailsService;
 
-    // using lambda
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http
+                //.cors() // Enable CORS
+                //.and()
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(antMatcher("/css/**")).permitAll()
                         .requestMatchers(antMatcher("/signup")).permitAll()
@@ -31,8 +32,7 @@ public class WebSecurityConfig {
                         .requestMatchers(antMatcher("/api/**")).permitAll()
                         .anyRequest().authenticated())
                 .headers(headers -> headers
-                        .frameOptions(frameoptions -> frameoptions.disable() // for h2 console
-                        ))
+                        .frameOptions(frameoptions -> frameoptions.disable())) // for h2 console
                 .formLogin(formlogin -> formlogin
                         .loginPage("/login")
                         .defaultSuccessUrl("/index", true)
@@ -47,4 +47,18 @@ public class WebSecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
+
+    // CORS Configuration
+    // Frontend rekisteröitymistä varten, ei toimi :'(
+
+    //@Bean
+    //public CorsConfigurationSource corsConfigurationSource() {
+    //    CorsConfiguration configuration = new CorsConfiguration();
+    //    configuration.addAllowedOrigin("*"); // Allow requests from any origin
+    //    configuration.addAllowedMethod("*"); // Allow all HTTP methods
+    //    configuration.addAllowedHeader("*"); // Allow all headers
+    //    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //    source.registerCorsConfiguration("/**", configuration);
+    //    return source;
+    // }
 }
